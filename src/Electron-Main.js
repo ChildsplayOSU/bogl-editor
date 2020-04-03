8aae8ac2a0cb4eed36b7af8ddd9a1c26af70b107
Kai Gay <gayk@oregonstate.edu>
2020-04-03 12:15:34 -0700

    hmm...

--- src/Electron-Main.js
+++ src/Electron-Main.js
@@ -1,6 +1,20 @@
-const {app, BrowserWindow} = require('electron')      
-function createWindow () {   
+
+
+
+const {app, BrowserWindow} = require('electron')
+const child_process = require('child_process')
+
+let SpielServer // Don't garbage collect the child process!
+
+function createWindow () {
 	win = new BrowserWindow({width: 800, height: 600});
 	win.loadURL('http://localhost:3000/');
-}      
-app.on('ready', createWindow)
+}
+function languageServer () {
+	SpielServer = child_process.spawn('./resources/spielserver'); // this probably needs to be different on windows...
+}
+app.on('ready', createWindow);
+//app.on('ready', languageServer);
+//app.on('will-quit', function () {
+//	SpielServer.kill();
+//});
