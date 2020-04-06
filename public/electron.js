dc821ea09b23feae6b3a36918a16c577bfce1f4f
Kai Gay <gayk@oregonstate.edu>
2020-04-06 13:13:00 -0700

    it works!

--- public/electron.js
+++ public/electron.js
@@ -1,3 +1,4 @@
+const path = require('path')
 
 
 
@@ -8,13 +9,14 @@ let SpielServer // Don't garbage collect the child process!
 
 function createWindow () {
 	win = new BrowserWindow({width: 800, height: 600});
-	win.loadURL('http://localhost:3000/');
+	//win.loadURL('http://localhost:3000/');
+	win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)
 }
 function languageServer () {
 	SpielServer = child_process.spawn('./resources/spielserver'); // this probably needs to be different on windows...
 }
 app.on('ready', createWindow);
-//app.on('ready', languageServer);
-//app.on('will-quit', function () {
-//	SpielServer.kill();
-//});
+  app.on('ready', languageServer);
+  app.on('will-quit', function () {
+  SpielServer.kill();
+});
