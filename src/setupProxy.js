bfd32085d53fed1dae7dcc53e563032b88f0fd1b
Benjamin Friedman <friedman.benjamin@gmail.com>
2020-05-21 22:00:27 -0700

    adds a proxy, making all requests a part of the frontend now

---
+++ src/setupProxy.js
@@ -0,0 +1,31 @@
+//
+// setupProxy.js
+//
+// This enables proxy-like behavior from React to forward requests
+// server-side to our local spielserver instance running on the same machine.
+// By keeping the requests local, we can avoid hitting anything that we don't want to
+//
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+
+  // test endpoint, verifies spielserver is running
+  app.use(
+    '/api_1/test',
+    createProxyMiddleware({
+      target: 'http://localhost:8080/test',
+      changeOrigin: true,
+      ignorePath: true
+    })
+  );
+
+  // attempts to run this code under the local instance
+  app.use(
+    '/api_1/runCode',
+    createProxyMiddleware({
+      target: 'http://localhost:8080/runCode',
+      changeOrigin: true,
+      ignorePath: true
+    })
+  );
+};
