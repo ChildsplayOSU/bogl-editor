f3a7528758c8fcd034bc2e40f99eff2ebdee45de
Benjamin Friedman <friedman.benjamin@gmail.com>
2020-10-04 14:09:56 -0700

    Updates navbar to temporarily remove examples, until we have new ones ready

--- src/Navbar/SpielNavbar.tsx
+++ src/Navbar/SpielNavbar.tsx
@@ -31,12 +31,14 @@ const SpielNavbar = (props) => {
 
     // examples available
     let boglExamples: Array<Array<string>> = [
-      ["Simplest Program","-- Simple Program example\ngame Simple\ntype Board = Array(1,1) of Int\ntype Input = Int"],
+      /* TODO BOGL Examples need to be updated
+      ["Simplest Program","-- Simple Program example\ngame Simple"],
       ["Function", "--example of a function\ngame FunctionExample\ntype Board = Array(1,1) of Int\ntype Input = Int\n\nf : Int\nf = 1"],
       ["Function with Parameter", "--example of a function that takes a parameter\ngame FunctionParamExample\ntype Board = Array(1,1) of Int\ntype Input = Int\n\n--adds 1 to whatever number we give this function\nf : Int -> Int\nf(x) = x + 1"],
       ["Board","--example of a simple board\ngame BoardExample\ntype Board = Array(3,3) of Int\ntype Input = Int\n\n--a board called 'ex' where every space is 0\nex : Board\nex!(x,y) = 0"],
       ["Simple Input","--example of taking in simple input\ngame SimpleInput\ntype Board = Array(1,1) of Int\ntype Input = Int\n\n--a function that adds 2 numbers together\nadd : Int -> Int\nadd(x) = let y = input in x + y"],
       ["Place a Piece","--example of placing a piece on a board\ngame PlacePiece\n\ntype Board = Array(3,3) of Int\ntype\nPosition = (Int,Int)\ntype Input = Position\n\n--a new board of 0's\nb : Board\nb!(x,y) = 0\n\n-- a function to place a piece on this board\nplacePiece : Board -> Board\nplacePiece(board) = let pos = input in place(1,board,pos)"]
+      */
     ];
 
     // Generate example list to be used in drop down
@@ -44,7 +46,7 @@ const SpielNavbar = (props) => {
         let navitems: Array<JSX.Element> = [];
         for(let i = 0; i < boglExamples.length; i++) {
           // TODO set this up so that the updatd examples populate back to the other router
-          navitems.push(<NavDropdown.Item onClick={() => props.setCode(boglExamples[i][1]) }>{ boglExamples[i][0] }</NavDropdown.Item>);
+          navitems.push(<NavDropdown.Item key={"boglexample-" + i.toString()} onClick={() => props.setCode(boglExamples[i][1]) }>{ boglExamples[i][0] }</NavDropdown.Item>);
         }
         return navitems;
     }
@@ -75,12 +77,12 @@ const SpielNavbar = (props) => {
       // reset button (if viewing shared file)
       if(params.p && params.s) {
         // viewing a shared file, show a 'Reset button for it'
-        items.push(<Button variant="outline-light" title="Reset your BoGL program to what was shared" type="button" onClick={(e) => props.reset()} className="right-padd">Reset</Button>);
+        items.push(<Button key="resetButton" variant="outline-light" title="Reset your BoGL program to what was shared" type="button" onClick={(e) => props.reset()} className="right-padd">Reset</Button>);
 
       }
 
       // share button
-      items.push(<Button variant="outline-light" title="Share your BoGL program" type="button" onClick={(e) => props.sharePrelude().then(function(result) {
+      items.push(<Button key="shareButton" variant="outline-light" title="Share your BoGL program" type="button" onClick={(e) => props.sharePrelude().then(function(result) {
         if(result[0].tag === "SpielSuccess") {
           // update that we shared
           setDidShare(true);
@@ -109,6 +111,15 @@ const SpielNavbar = (props) => {
     }
 
     // Uses React-Router to change page
+
+    // TODO after the 'Themes' dropdown, this can be added
+    // when the examples are ready.
+    // ALSO needs to uncomment the updated examples afterwards
+    /*
+    <NavDropdown title="Examples" id="basic-nav-dropdown">
+        {getBoGLExamples()}
+    </NavDropdown>
+    */
     return (
         <Navbar bg="danger" variant="dark" expand="lg" fixed="top">
             <Navbar.Brand>BoGL: Board Game Language</Navbar.Brand>
@@ -119,9 +130,7 @@ const SpielNavbar = (props) => {
                     <NavDropdown title="Themes" id="basic-nav-dropdown">
                         {getThemes()}
                     </NavDropdown>
-                    <NavDropdown title="Examples" id="basic-nav-dropdown">
-                        {getBoGLExamples()}
-                    </NavDropdown>
+
                 </Nav>
                 <Form inline>
                   {getShareOption()}
